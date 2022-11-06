eddcc649cd227537b25f4767f384da173f7dc6e0
lsy0xcc <lsy0xcc@outlook.com>
2022-11-06 22:03:30 +0800

    Add test API

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get(['', 'hello-world'])
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('long-time-hello')
+  async getLongTimeHello(): Promise<string> {
+    return this.appService.getLongTimeHello();
+  }
+
+  @Get('hello-user')
+  async getHelloUser(@Query('name') name: string): Promise<string> {
+    console.log(name);
+    return this.appService.getHelloUser(name);
+  }
+
+  @Get('users')
+  getUserList(): any[] {
+    return this.appService.getUserList();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,38 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getUserList() {
+    return [
+      {
+        id: 1,
+        name: 'Jane',
+      },
+      {
+        id: 2,
+        name: 'John',
+      },
+    ];
+  }
+
+  getLongTimeHello(time = 1000): Promise<string> {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        const ran = Math.random();
+        if (ran > 0.3) {
+          res(`hello ${ran.toFixed(2)}`);
+        } else {
+          rej(`fake error ${ran.toFixed(2)}`);
+        }
+      }, time);
+    });
+  }
+
+  getHelloUser(input = ''): Promise<string> {
+    return new Promise((res) => {
+      setTimeout(() => {
+        res(input + ', hello!');
+      }, Math.random() * 1000);
+    });
+  }
 }
